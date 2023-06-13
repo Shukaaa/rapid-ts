85064503d9904f66242e4d52e4adeec26cff385d
Shukaaa <asappokimane@gmail.com>
2023-06-13 18:29:47 +0200

    trying to create npm package

--- index.ts
+++ index.ts
@@ -1,7 +1,1 @@
-import {RapidServer} from "./src/RapidServer";
-
-let server: RapidServer = new RapidServer();
-server.start();
-
-//server.restart() // Restarts the server
-//server.close() // Closes the server
+export { RapidServer } from "./src/RapidServer"
